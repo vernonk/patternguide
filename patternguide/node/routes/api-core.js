bbad1a05d8816b3200f93d13cba47180925bf1ca
Vernon Kesner <vernonkesner@gmail.com>
2015-04-05 10:41:48 -0400

    Initial API work for elements as base use case

--- patternguide/node/routes/api-core.js
+++ patternguide/node/routes/api-core.js
@@ -5,123 +5,127 @@
 * The routes are set up using `router.verb` methods where the verb is an
 * HTTP verb (e.g. router.get(), router.post())
 **/
-var express = require( "express" ),
+var fs = require( "fs" ),
+    path = require( "path" ),
+    _ = require( "lodash" ),
+    express = require( "express" ),
     router = express.Router();
-
-    // middleware specific to this router
-    // router.use(function timeLog(req, res, next) {
-    //   console.log('Time: ', Date.now());
-    //   next();
-    // })
-
-// Clean this up... just stubbing out some paths here.
-
 
 // Could use named routes to make updating application code easier
 // Instead, I opted to simple define the API simple (rather than definining a
-// bunch of custom named routes -- unless I misunderstand it which I could)
+// bunch of custom named routes -- unless I misunderstand it which I could have)
 
-// Elements
-router.post( "/element/:name", function ( req, res ) {
+//elements
+//modules
+//patterns
+//layouts
+//comps
+//wires
 
-  res.send( "Stubbing it out" );
+const IGNORE = [ ".DS_Store" ];
 
+// Elements base, return full list
+router.get( "/elements", function ( req, res ) {
+
+  var resp = [],
+      elementGroups = _.remove( fs.readdirSync( path.join( __dirname, "..", "..", "..", "src/elements" ) ),
+                                function ( dir ) {
+                                  return IGNORE.indexOf( dir ) === -1;
+                                });
+
+  for ( var i = 0, l = elementGroups.length; i < l; i++ ) {
+    resp[ i ] = {};
+    resp[ i ].id = i;
+    resp[ i ].name = elementGroups[ i ];
+    resp[ i ].items = _.remove( fs.readdirSync( path.join( __dirname, "..", "..", "..", "src/elements", elementGroups[ i ] ) ),
+                              function ( dir ) {
+                                return IGNORE.indexOf( dir ) === -1;
+                              });
+  }
+
+  res.json( resp );
 });
 
-router.get( "/element/:name", function ( req, res ) {
+// Element group page, another hub but more filtered
+router.get( "/elements/:group", function ( req, res ) {
+  var resp = [],
+      elementGroups = _.remove( fs.readdirSync( path.join( __dirname, "..", "..", "..", "src/elements", req.params.group ) ),
+                                function ( dir ) {
+                                  return IGNORE.indexOf( dir ) === -1;
+                                });
 
-  res.send( "Stubbing it out" );
+  for ( var i = 0, l = elementGroups.length; i < l; i++ ) {
+    resp[ i ] = {};
+    resp[ i ].id = i;
+    resp[ i ].name = elementGroups[ i ];
+    resp[ i ].group = req.params.group;
+  }
 
+  res.json( resp );
 });
 
-router.put( "/element/:name", function ( req, res ) {
+// Get the details about a specific element, render just the MD?
+// Look for examples dir and iframe in?
+router.get( "/elements/:group/:item", function ( req, res ) {
 
-  res.send( "Stubbing it out" );
+  var pathFromRoot = path.join( __dirname, "..", "..", "..", "src/elements", req.params.group, req.params.item ),
+      paths = {
+        readme: path.join( pathFromRoot, "readme.md" ),
+        config: path.join( pathFromRoot, "config.json" ),
+        styles: path.join( pathFromRoot, "styles", req.params.item + ".scss" ),
+        js: path.join( pathFromRoot, "js", req.params.item + ".js" ),
+        tests: path.join( pathFromRoot, "js", "tests", "spec.js" ),
+      },
+      resp = {
+        id: req.params.item,
+        css: null,
+        js: null,
+        tests: null,
+        partial: null
+      };
 
-});
+  // get the readme
+  if ( fs.existsSync( paths.readme ) ) {
+    resp.readme = fs.readFileSync( paths.readme, { encoding: "utf8" } );
+  } else {
+    resp.readme = null;
+  }
 
-router.delete( "/element/:name", function ( req, res ) {
+  // get the config
+  if ( fs.existsSync( paths.config ) ) {
+    resp.config = fs.readFileSync( paths.config, { encoding: "utf8" } );
+  } else {
+    resp.config = null;
+  }
 
-  res.send( "Stubbing it out" );
+  // do we have a partial?
+  if ( fs.existsSync( paths.partial ) ) {
+    resp.partial = fs.readFileSync( paths.partial, { encoding: "utf8" } );
+  }
 
-});
+  // do we have any styles?
+  if ( fs.existsSync( paths.styles ) ) {
+    paths.styles.replace( /(?:src)([A-Za-z\-\/]+)/, function ( full, path ) {
+      resp.css = path + ".css";
+    });
+  }
 
-// Modules
-router.post( "/module/:name", function ( req, res ) {
+  // do we have any functionality?
+  if ( fs.existsSync( paths.js ) ) {
+    paths.js.replace( /(?:src)([A-Za-z\-\/\.]+)/, function ( full, path ) {
+      resp.js = path;
+    });
+  }
 
-  res.send( "Stubbing it out" );
+  // do we have any unit tests?
+  if ( fs.existsSync( paths.tests ) ) {
+    paths.tests.replace( /(?:src)([A-Za-z\-\/\.]+)/, function ( full, path ) {
+      resp.tests = path;
+    });
+  }
 
-});
+  res.json( resp );
+})
 
-router.get( "/api/module/:name", function ( req, res ) {
 
-  res.send( "Stubbing it out" );
-
-});
-
-router.put( "/module/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-router.delete( "/module/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-// Patterns
-router.post( "/pattern/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-router.get( "/pattern/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-router.put( "/pattern/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-router.delete( "/pattern/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-// Layouts
-router.post( "/layout/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-router.get( "/layout/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-  // code down here happens on the back loop after response is sent to clients
-
-});
-
-router.put( "/layout/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-router.delete( "/layout/:name", function ( req, res ) {
-
-  res.send( "Stubbing it out" );
-
-});
-
-// Finally export our router
 module.exports = router;
